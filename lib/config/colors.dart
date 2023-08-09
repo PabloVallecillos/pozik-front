1ad3ce59ace93b19037ce9fcaa45b808a51ea86e
PabloVallecillos <vallecillospablo@gmail.com>
2023-08-09 23:36:32 +0200

    feat(welcome): Implemented.
    refs: #865cn5u6y

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 /// colors
-const Color primary = Color(0xffffb100);
+const Color primary = Color(0xff368c8c);
 const Color red = Color(0xffF9612F);
 const Color secondary = Color(0xffffb100);
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
+import 'dart:ui';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:pozik_front/config/firebase/firebase_options.dart';
-import 'package:pozik_front/utils/extensions/string.dart';
+import 'package:pozik_front/screens/welcome_screen.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:sentry_logging/sentry_logging.dart';
 
@@ -16,7 +18,7 @@ Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load();
   await SentryFlutter.init((SentryFlutterOptions options) {
-    options.dsn = isProduction ? dotenv.get('SENTRY_DSN') : dotenv.get('SENTRY_DSN');
+    options.dsn = isProduction ? dotenv.get('SENTRY_DSN') : '';
     options.tracesSampleRate = isProduction ? 0.6 : 1.0;
     options.autoAppStart = false;
     options.attachThreads = true;
@@ -44,20 +46,20 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver  {
 
   late final ValueNotifier<Brightness> _themeModeNotifier = ValueNotifier<Brightness>(Brightness.light);
   late final WidgetsBinding _widgetBinding;
-  late final dynamic _window;
+  late final PlatformDispatcher _platform;
 
   @override
   void initState() {
     _widgetBinding = WidgetsBinding.instance;
     _widgetBinding.addObserver(this);
-    _window = _widgetBinding.window;
-    _themeModeNotifier.value = _window.platformDispatcher.platformBrightness;
+    _platform = _widgetBinding.platformDispatcher;
+    _themeModeNotifier.value = _platform.platformBrightness;
     super.initState();
   }
 
   @override
   void didChangePlatformBrightness() {
-    _themeModeNotifier.value = _window.platformDispatcher.platformBrightness;
+    _themeModeNotifier.value = _platform.platformBrightness;
     super.didChangePlatformBrightness();
   }
 
@@ -80,98 +82,9 @@ class _MyAppState extends State<MyApp> with WidgetsBindingObserver  {
             Locale('en', ''),
             Locale('es', ''),
           ],
-          home: const MyHomePage(title: 'Flutter Demo Home Page'),
+          home: const WelcomeScreen(),
         );
       }
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  Future<void> _incrementCounter() async {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(AppLocalizations.of(context).password.capitalize(), style: const TextStyle(color: Colors.black),),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
